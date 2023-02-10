e64e259da30127d7fc3bfec702060656cfd5553d
Ibrahimad98 <ibrahim.ad90@gmail.com>
2023-02-10 10:34:38 +0700

    initial commit

---
+++ bintoro-app/src/components/SosmedComponent.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  name: "SosmedComponent",
+};
+</script>
+<template>
+  <nav style="background-color: #ff8a00" class="">
+    <p>Hello World</p>
+  </nav>
+</template>
---
+++ bintoro-app/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script>
+import { mapState, mapActions } from "pinia";
+import SosmedComponent from "../components/SosmedComponent.vue";
+
+export default {
+  name: "HomeView",
+  computed: {
+    // ...mapState(useMovieStore, []),
+  },
+  data() {
+    return {};
+  },
+  components: {
+    SosmedComponent,
+  },
+  methods: {
+    // ...mapActions(useMovieStore, []),
+  },
+  created() {},
+};
+</script>
+<template>
+  <SosmedComponent />
+</template>
